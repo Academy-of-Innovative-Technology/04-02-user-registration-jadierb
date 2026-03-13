0271375a2fe7f89ebf7f48e6fa6e2c5a9f4680b1
jadier <jadierb@nycstudents.net>
2026-03-13 00:10:48 +0000

    display spans

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,12 @@ document.querySelector(".btn-primary").addEventListener("click", function (event
     const passwordInput = document.getElementById("password");
     const countrySelect = document.getElementById("country");
     const aboutTextarea = document.getElementById("about");
-    
+
+    const savedFirstNameSpan = document.getElementById("savedFirstName");
+    const savedLastNameSpan = document.getElementById("savedLastName");
+    const savedEmailSpan = document.getElementById("savedEmail");
+    const savedCountrySpan = document.getElementById("savedCountry");
+    const savedAccountTypeSpan = document.getElementById("savedAboutType");
+    const savedAboutSpan = document.getElementById("savedAbout");
 
 });
