0f93eb08a9228c53ff1d89d3e676796f69446084
jadier <jadierb@nycstudents.net>
2026-03-13 00:00:55 +0000

    form inputs

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,12 @@ document.querySelector(".btn-primary").addEventListener("click", function (event
     const loadUserBtn = document.getElementById("loadUserBtn");
     const clearUserBtn = document.getElementById("clearUserBtn");
 
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const countrySelect = document.getElementById("country");
+    const aboutTextarea = document.getElementById("about");
+    
+
 });
